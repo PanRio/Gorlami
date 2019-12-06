64a5ed7a5022cdf5896fb076f5877b846062d9a6
Rafał <rafalmiskiewicz9@gmail.com>
2019-12-07 00:12:05 +0100

    ghostt

---
+++ Gorlami/Assets/Scripts/ghost.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghost : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform ghostt;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ghostt.transform.localPosition = new Vector3(player.transform.localPosition.x,player.transform.localPosition.y,player.transform.localPosition.z+player.GetComponent<moveToAnotherSide>().distanceBetweenScean); //SetParent localScale.Set(0,4,0);    
+    }
+}
+
