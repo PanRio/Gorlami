aa615cbfc5daeda34b19131dd4840b3e0fb78b92
Roberto <roberto.laczak@gmail.com>
2019-12-07 01:01:44 +0100

    Yes

---
+++ Gorlami/Assets/Scripts/ObjectInteract.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteract : MonoBehaviour
+{
+    CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G)) controller.transform.SetPositionAndRotation(controller.transform.position * 2, controller.transform.rotation);
+
+
+
+    }
+}
