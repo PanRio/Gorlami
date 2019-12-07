d7573ef11a7bd035158821b088ae2c0fb6ed81e7
Rafał <rafalmiskiewicz9@gmail.com>
2019-12-07 05:02:26 +0100

    ghost working

--- Gorlami/Assets/Scripts/moveToAnotherSide.cs
+++ Gorlami/Assets/Scripts/moveToAnotherSide.cs
@@ -7,28 +7,44 @@ public class moveToAnotherSide : MonoBehaviour
 
     public bool isDarkState = false;//1-ciemny . 0-jasny
     public Transform player;
-    public float distanceBetweenScean = 11f;
-    // Start is called before the first frame update
-    void Start() {
+    public Transform ghost;
+    bool isInWall=false;
+    public LayerMask groundMask;
 
-    }
+    public float distanceBetweenScean = 11f;
+
 
     // Update is called once per frame
     void LateUpdate() {
+        
+
         if(Input.GetKeyDown(KeyCode.E)) {
-            
-            if(isDarkState) {
-                isDarkState = false;
-                player.localPosition = new Vector3(player.localPosition.x,player.localPosition.y,player.localPosition.z - distanceBetweenScean);
-    
-            }
-
-            else {
-                isDarkState = true;
-                player.localPosition = new Vector3(player.localPosition.x,player.localPosition.y,player.localPosition.z + distanceBetweenScean);
+            if(Physics.CheckSphere(ghost.position,0.5f,groundMask)) {
+                //Physics.CheckSphere()
+                //Physics.CheckCapsule()
                 
+                Debug.Log("Hit");
             }
+            else {
 
+                if(isDarkState) {
+                    isDarkState = false;
+                    player.localPosition = new Vector3(player.localPosition.x,player.localPosition.y,player.localPosition.z - distanceBetweenScean);
+
+                }
+
+                else {
+                    isDarkState = true;
+                    player.localPosition = new Vector3(player.localPosition.x,player.localPosition.y,player.localPosition.z + distanceBetweenScean);
+
+                }
+            }
+        }
+        if(isDarkState) {
+            ghost.localPosition = new Vector3(player.localPosition.x,player.localPosition.y,player.localPosition.z - distanceBetweenScean);
+        }
+        else {
+            ghost.localPosition = new Vector3(player.localPosition.x,player.localPosition.y,player.localPosition.z + distanceBetweenScean);
         }
     }
     /*
@@ -47,3 +63,4 @@ public class moveToAnotherSide : MonoBehaviour
     }
     */
 }
+
